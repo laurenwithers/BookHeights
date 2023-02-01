2e06c0490f7afe3f47286852f193a3326411de04
laurenwithers <laurenwithers@hotmail.co.nz>
2023-02-02 08:56:13 +1300

    commit

--- App.js
+++ App.js
@@ -1,7 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import{Router} from './app/router';
+
 
 export default function App() {
+
+  console.log("App Excecuted");
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
@@ -13,7 +18,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'dodgerblue',
     alignItems: 'center',
     justifyContent: 'center',
   },
